4e2af4279d026bdc138f6179630a45a41ed8edfd
darinbtw <123627549+darinbtw@users.noreply.github.com>
2023-11-29 13:16:34 +0300

    update

--- auto_cliker_with_jump.py
+++ auto_cliker_with_jump.py
@@ -10,53 +10,45 @@ class AutoClicker:
     def __init__(self, interval=0.20, hotkey_start="9", hotkey_stop="0"):
         self.interval = interval
         self.running = False
+        self.jump_disabled = True  # Начальное значение для отключения прыжка
+        self.jump_triggered = False
         self.hotkey_start = KeyCode.from_char(hotkey_start)
         self.hotkey_stop = KeyCode.from_char(hotkey_stop)
         self.mouse = MouseController()
         self.auto_clicker_thread = None
-        self.jump_disabled = False  # Флаг для отключения прыжка
 
     def start_clicking(self):
         self.running = True
         while self.running:
             self.mouse.click(Button.left)
-            if not self.jump_disabled:  # Добавлено условие для прыжка
+            if not self.jump_disabled and not self.jump_triggered:
                 keyboard.press_and_release('space')
+                self.jump_triggered = True
             time.sleep(self.interval)
 
     def stop_clicking(self):
         self.running = False
 
-def disable_jump():
-    auto_clicker.jump_disabled = True
-
-def enable_jump():
-    auto_clicker.jump_disabled = False
-
 def start_auto_clicker():
+    auto_clicker.jump_triggered = False
+    auto_clicker.jump_disabled = False  # Разрешаем прыжок при старте AutoClicker
     auto_clicker.auto_clicker_thread = threading.Thread(target=auto_clicker.start_clicking)
     auto_clicker.auto_clicker_thread.start()
     status_var.set("Running")
-
-    # Отключаем прыжок на 5 секунд
-    disable_jump_timer = threading.Timer(5, disable_jump)
-    disable_jump_timer.start()
-
-    # Включаем прыжок после 5 секунд
-    enable_jump_timer = threading.Timer(10, enable_jump)
-    enable_jump_timer.start()
 
 def stop_auto_clicker():
     auto_clicker.stop_clicking()
     if auto_clicker.auto_clicker_thread is not None:
         auto_clicker.auto_clicker_thread.join()
+    auto_clicker.jump_disabled = True  # Отключаем прыжок при остановке
     status_var.set("Stopped")
 
 def press_space_periodically():
     try:
         active_window = gw.getActiveWindow()
         if active_window:
-            keyboard.press_and_release('space')
+            if not auto_clicker.jump_disabled:
+                keyboard.press_and_release('space')
     except Exception as e:
         print(f"Error: {e}")
 
@@ -85,6 +77,6 @@ with KeyboardListener(on_release=on_key_release):
     status_label = tk.Label(root, textvariable=status_var)
     status_label.pack()
 
-    root.after(60, press_space_periodically)
+    root.after(10, press_space_periodically)
 
     root.mainloop()
